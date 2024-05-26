69d9ed190e3ae20b63a5809ee7502ab00922b3aa
JosemaRodas <114843092+JosemaRodas@users.noreply.github.com>
2024-05-26 01:01:29 -0400

    git commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,22 @@ const currentYear = new Date().getFullYear();
 copyrightElement.textContent = `Copyright © ${currentYear} 🌴 Jose Manuel Rodas 🌴 La Paz, Bolivia`;
 
 lastModifiedElement.textContent = `Document last modified: ${document.lastModified}`;
+
+
+
+const temperature = 20; 
+const windSpeed = 5; 
+
+const windchillFormula = (temp, windSpeed) => {
+  return 13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16);
+};
+
+let windChill;
+if (temperature <= 10 && windSpeed > 4.8) {
+  windChill = windchillFormula(temperature, windSpeed).toFixed(1); 
+} else {
+  windChill = "N/A";
+}
+
+const windChillElement = document.getElementById("wind-chill");
+windChillElement.textContent = windChill + "°C";
